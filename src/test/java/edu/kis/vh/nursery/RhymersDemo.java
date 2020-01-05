6e77f43ab9c5ec44c3daa5370c97063f34396f52
Dawid Bukała <pk-31@sr.dmcs.pl>
2020-01-05 23:56:28 +0100

    Akcesoria i Hermetyzacja

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,40 +1,37 @@
 package edu.kis.vh.nursery;
 
-import edu.kis.vh.nursery.defaultCountingOutRhymer;
-import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 class RhymersDemo {
 
+    public static final int SIZE1 = 15;
+    public static final int SIZE2 = 3;
+    public static final int BOUND = 20;
 
-	public static final int SIZE1 = 15;
-	public static final int BOUND = 20;
-	public static final int SIZE2 = 3;
+    public static void main(String[] args) {
+        Rhymersfactory factory = new DefaultRhymersFactory();
 
-	public static void main(String[] args) {
-		Rhymersfactory factory = new DefaultRhymersFactory();
-		
-		defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-				factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
-		
-		for (int i = 1; i < SIZE1; i++)
-			for (int j = 0; j < SIZE2; j++)
-				rhymers[j].countIn(i);
-		
-		java.util.Random rn = new java.util.Random();
-		for (int i = 1; i < SIZE1; i++)
-			rhymers[SIZE2].countIn(rn.nextInt(BOUND));
-		
-		for (int i = 0; i < rhymers.length; i++) {
-			while (!rhymers[i].callCheck())
-				System.out.print(rhymers[i].countOut() + "  ");
-			System.out.println();
-		}
-		
-		System.out.println("total rejected is "
-				+ ((HanoiRhymer) rhymers[SIZE2]).reportRejected());
-		
-	}
-	
+        DefaultCountingOutRhymer[] rhymers = {factory.GetStandardRhymer(), factory.GetFalseRhymer(),
+                factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
+
+        for (int i = 1; i < SIZE1; i++)
+            for (int j = 0; j < SIZE2; j++)
+                rhymers[j].countIn(i);
+
+        java.util.Random rn = new java.util.Random();
+        for (int i = 1; i < SIZE1; i++)
+            rhymers[3].countIn(rn.nextInt(BOUND));
+
+        for (int i = 0; i < rhymers.length; i++) {
+            while (!rhymers[i].callCheck())
+                System.out.print(rhymers[i].countOut() + "  ");
+            System.out.println();
+        }
+
+        System.out.println("total rejected is "
+                + ((HanoiRhymer) rhymers[SIZE2]).reportRejected());
+
+    }
+
 }
